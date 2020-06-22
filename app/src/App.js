c7c1ebd5d3cf28617385d69ad9c088ec12e02970
Big Chungus <iatechristmas@gmail.com>
2020-06-22 16:57:38 -0700

    added login form and axioswithauth

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

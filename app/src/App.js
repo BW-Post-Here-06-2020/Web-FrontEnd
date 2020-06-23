5d0133a702f456b869469c528923df966ca2b338
Big Chungus <iatechristmas@gmail.com>
2020-06-23 12:00:45 -0700

    added dashboard and added private route to it

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+          <PrivateRoute exact path="/dashboard" compontent={Dashboard} />
           <Route exact path="/" component={Login} />
         </Switch>
       </div>

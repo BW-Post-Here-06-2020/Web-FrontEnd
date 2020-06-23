ba867580e6d5ecdfde8813346ebb991a1a38a853
Big Chungus <iatechristmas@gmail.com>
2020-06-23 15:31:33 -0700

    added nav bar

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Dashboard from "./components/Dashboard";
+import ButtonAppBar from "./components/NavBar/ButtonAppBar";
 
 function App() {
   return (
     <Router>
       <div>
+        <ButtonAppBar />
         <Switch>
           <PrivateRoute exact path="/dashboard" compontent={Dashboard} />
           <Route exact path="/" component={Login} />
--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -1,3 +1,9 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getData } from "../actions/index";
+
 const Dashboard = () => {
   return null;
 };
